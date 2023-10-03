402a56f3340fe46e1e57c0b7ee528d56df00aa96
masternati <sohrabhossain207@gmail.com>
2023-10-03 17:21:36 +0600

    Total Expenses calculation is complete for food, rent and clothes

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+document.getElementById('calculate').addEventListener('click', function(){
+    const income = document.getElementById('income-amount');
+    const incomeAmountString = income.value;
+    const incomeAmount = parseInt(incomeAmountString);
+
+    const food = document.getElementById('food');
+    const foodString = food.value;
+    const foodAmount = parseInt(foodString);
+
+    const rent = document.getElementById('rent');
+    const rentString = rent.value;
+    const rentAmount = parseInt(rentString);
+
+    const clothes = document.getElementById('clothes');
+    const clothString = clothes.value;
+    const clothAmount = parseInt(clothString);
+
+    const expense = document.getElementById('total-expenses');
+    const calculateExpenses = foodAmount + rentAmount + clothAmount;
+    expense.innerText = calculateExpenses;
+
+
+});
+
+
+
